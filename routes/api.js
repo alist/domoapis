1abf04c4a99597eab480c76c58ed85ae8c0a6f4b
Shirish Kamath <shirishk.87@gmail.com>
2013-08-18 21:13:11 +0530

    API routes for org

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ module.exports.public = function(app){
   app.get(apiConfig.path + '/organizations', OrganizationController.getAll.bind(OrganizationController));
 
   // TODO: make private
+  app.get(apiConfig.path + '/organizations/:organization', OrganizationController.getInfo.bind(OrganizationController));
   app.get(apiConfig.path + '/organizations/:organization/users', OrganizationController.getUsersByOrgId.bind(OrganizationController));
-  app.get(apiConfig.path + '/organizations/:organization/users/:userId', OrganizationController.getUser.bind(OrganizationController));
+  app.get(apiConfig.path + '/organizations/:organization/users/:userId', OrganizationController.getUsersByOrgId.bind(OrganizationController));
 }
