c644086b57e22cf62230b24ffcca63afc2b376ab
Shirish Kamath <shirishk.87@gmail.com>
2013-08-19 19:08:54 +0530

    hashbang

--- deploy/presetup.sh
+++ deploy/presetup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo 'Setting up SSH'
 export SSH_CONFIG=~/.ssh/config; mkdir -p ~/.ssh; touch "$SSH_CONFIG"; if !(grep -q "Host bitbucket.org" "$SSH_CONFIG"); then echo -e "Host bitbucket.org\n\tStrictHostKeyChecking no\n" >> $SSH_CONFIG; fi
 
