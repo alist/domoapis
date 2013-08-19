764a497b046cb3524b1e32fda4d34fc6830abe00
Shirish Kamath <shirishk.87@gmail.com>
2013-08-19 22:11:14 +0530

    Weird npm issue #3664

--- deploy/presetup.sh
+++ deploy/presetup.sh
@@ -2,3 +2,4 @@
 echo "ENV: APP_ROOT: $APP_ROOT"
 echo 'Setting up SSH'
 export SSH_CONFIG=~/.ssh/config; mkdir -p ~/.ssh; touch "$SSH_CONFIG"; if !(grep -q "Host bitbucket.org" "$SSH_CONFIG"); then echo -e "Host bitbucket.org\n\tStrictHostKeyChecking no\n" >> $SSH_CONFIG; fi
+rm -rf ~/.npm
