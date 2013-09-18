2e6c73d7693a9e78815e7f8c89cf84e9b0f91b5f
Harish N Kamath <harishnk@airtight.local>
2013-09-18 18:42:53 -0400

    changed organizationID to id on test data file

--- test/data/orgData.js
+++ test/data/orgData.js
@@ -1,6 +1,6 @@
 module.exports.orgData = [
   {
-    "organizationID": "1",
+    "id": "1",
     "displayName": "Massachusetts Institute of Technology",
     "orgURL": "mit",
     "city": "Cambridge",
@@ -9,63 +9,63 @@ module.exports.orgData = [
     "code": "mit5"
   },
   {
-    "organizationID": "2",
+    "id": "2",
     "displayName": "Convallis LLC",
     "orgURL": "convallisllc",
     "city": "Aurora",
     "region": "Colorado"
   },
   {
-    "organizationID": "3",
+    "id": "3",
     "displayName": "Ullamcorper Consulting",
     "orgURL": "ullamcorperconsulting",
     "city": "New Orleans",
     "region": "Louisiana"
   },
   {
-    "organizationID": "4",
+    "id": "4",
     "displayName": "Ipsum LLC",
     "orgURL": "ipsumllc",
     "city": "Joliet",
     "region": "IL"
   },
   {
-    "organizationID": "5",
+    "id": "5",
     "displayName": "Massachusetts Institute of Technology",
     "orgURL": "mit",
     "city": "Pittsburgh",
     "region": "PA"
   },
   {
-    "organizationID": "6",
+    "id": "6",
     "displayName": "Lorem Company",
     "orgURL": "loremcompany",
     "city": "Athens",
     "region": "Georgia"
   },
   {
-    "organizationID": "7",
+    "id": "7",
     "displayName": "Purus Ac Tellus LLP",
     "orgURL": "purusac",
     "city": "Owensboro",
     "region": "Kentucky"
   },
   {
-    "organizationID": "8",
+    "id": "8",
     "displayName": "Euismod LLP",
     "orgURL": "euismodllp",
     "city": "Tucson",
     "region": "AZ"
   },
   {
-    "organizationID": "9",
+    "id": "9",
     "displayName": "Arcu Eu Odio Industries",
     "orgURL": "arcueu",
     "city": "Columbia",
     "region": "MD"
   },
   {
-    "organizationID": "10",
+    "id": "10",
     "displayName": "Lorem Eu Metus Corporation",
     "orgURL": "loremeu",
     "city": "Kaneohe",
@@ -78,630 +78,630 @@ module.exports.orgData = [
 
 
 module.exports.orgData2 = [{
-    "organizationID": "11",
+    "id": "11",
     "displayName": "Venenatis Lacus Ltd",
     "orgURL": "venenatislacus",
     "city": "Portland",
     "region": "ME"
   },
   {
-    "organizationID": "12",
+    "id": "12",
     "displayName": "Aliquam Erat Ltd",
     "orgURL": "aliquamerat",
     "city": "New Orleans",
     "region": "LA"
   },
   {
-    "organizationID": "13",
+    "id": "13",
     "displayName": "In Tincidunt LLP",
     "orgURL": "intincidunt",
     "city": "Iowa City",
     "region": "Iowa"
   },
   {
-    "organizationID": "14",
+    "id": "14",
     "displayName": "Proin Foundation",
     "orgURL": "proinfoundation",
     "city": "Cedar Rapids",
     "region": "IA"
   },
   {
-    "organizationID": "15",
+    "id": "15",
     "displayName": "Orci Phasellus Associates",
     "orgURL": "orciphasellus",
     "city": "Dallas",
     "region": "Texas"
   },
   {
-    "organizationID": "16",
+    "id": "16",
     "displayName": "Rutrum Lorem Ac Limited",
     "orgURL": "rutrumlorem",
     "city": "Clarksville",
     "region": "Tennessee"
   },
   {
-    "organizationID": "17",
+    "id": "17",
     "displayName": "Et Pede Nunc Consulting",
     "orgURL": "etpede",
     "city": "Reading",
     "region": "Pennsylvania"
   },
   {
-    "organizationID": "18",
+    "id": "18",
     "displayName": "Ultrices Posuere Cubilia PC",
     "orgURL": "ultricesposuere",
     "city": "Minneapolis",
     "region": "Minnesota"
   },
   {
-    "organizationID": "19",
+    "id": "19",
     "displayName": "Mauris LLC",
     "orgURL": "maurisllc",
     "city": "Rockford",
     "region": "Illinois"
   },
   {
-    "organizationID": "20",
+    "id": "20",
     "displayName": "Dolor Dolor Tempus Associates",
     "orgURL": "dolordolor",
     "city": "Birmingham",
     "region": "Alabama"
   },
   {
-    "organizationID": "21",
+    "id": "21",
     "displayName": "Dis Parturient Montes Industries",
     "orgURL": "disparturient",
     "city": "Lansing",
     "region": "MI"
   },
   {
-    "organizationID": "22",
+    "id": "22",
     "displayName": "Sit Amet PC",
     "orgURL": "sitamet",
     "city": "Minneapolis",
     "region": "Minnesota"
   },
   {
-    "organizationID": "23",
+    "id": "23",
     "displayName": "Blandit Mattis Cras Incorporated",
     "orgURL": "blanditmattis",
     "city": "Allentown",
     "region": "PA"
   },
   {
-    "organizationID": "24",
+    "id": "24",
     "displayName": "Neque Industries",
     "orgURL": "nequeindustries",
     "city": "Bellevue",
     "region": "WA"
   },
   {
-    "organizationID": "25",
+    "id": "25",
     "displayName": "Nulla At Industries",
     "orgURL": "nullaat",
     "city": "Sandy",
     "region": "UT"
   },
   {
-    "organizationID": "26",
+    "id": "26",
     "displayName": "Ridiculus Institute",
     "orgURL": "ridiculusinstitute",
     "city": "Bozeman",
     "region": "Montana"
   },
   {
-    "organizationID": "27",
+    "id": "27",
     "displayName": "Lectus Nullam Suscipit Industries",
     "orgURL": "lectusnullam",
     "city": "Auburn",
     "region": "Maine"
   },
   {
-    "organizationID": "28",
+    "id": "28",
     "displayName": "Gravida Mauris Ut Inc.",
     "orgURL": "gravidamauris",
     "city": "Southaven",
     "region": "Mississippi"
   },
   {
-    "organizationID": "29",
+    "id": "29",
     "displayName": "Pede Cras Foundation",
     "orgURL": "pedecras",
     "city": "Tuscaloosa",
     "region": "AL"
   },
   {
-    "organizationID": "30",
+    "id": "30",
     "displayName": "Lacus Aliquam LLC",
     "orgURL": "lacusaliquam",
     "city": "Cincinnati",
     "region": "Ohio"
   },
   {
-    "organizationID": "31",
+    "id": "31",
     "displayName": "Ut Sagittis Lobortis Inc.",
     "orgURL": "utsagittis",
     "city": "Shreveport",
     "region": "Louisiana"
   },
   {
-    "organizationID": "32",
+    "id": "32",
     "displayName": "Sed LLC",
     "orgURL": "sedllc",
     "city": "Bridgeport",
     "region": "CT"
   },
   {
-    "organizationID": "33",
+    "id": "33",
     "displayName": "Venenatis Industries",
     "orgURL": "venenatisindustries",
     "city": "Los Angeles",
     "region": "CA"
   },
   {
-    "organizationID": "34",
+    "id": "34",
     "displayName": "Mauris Elit Limited",
     "orgURL": "mauriselit",
     "city": "Boston",
     "region": "MA"
   },
   {
-    "organizationID": "35",
+    "id": "35",
     "displayName": "Velit Eget Ltd",
     "orgURL": "veliteget",
     "city": "Little Rock",
     "region": "AK"
   },
   {
-    "organizationID": "36",
+    "id": "36",
     "displayName": "Nunc Id Foundation",
     "orgURL": "nuncid",
     "city": "Kapolei",
     "region": "Hawaii"
   },
   {
-    "organizationID": "37",
+    "id": "37",
     "displayName": "Imperdiet Dictum Magna Consulting",
     "orgURL": "imperdietdictum",
     "city": "Olathe",
     "region": "KS"
   },
   {
-    "organizationID": "38",
+    "id": "38",
     "displayName": "Est Ac Consulting",
     "orgURL": "estac",
     "city": "Essex",
     "region": "VT"
   },
   {
-    "organizationID": "39",
+    "id": "39",
     "displayName": "Arcu Corporation",
     "orgURL": "arcucorporation",
     "city": "Los Angeles",
     "region": "CA"
   },
   {
-    "organizationID": "40",
+    "id": "40",
     "displayName": "Dolor Elit Pellentesque Incorporated",
     "orgURL": "dolorelit",
     "city": "Atlanta",
     "region": "Georgia"
   },
   {
-    "organizationID": "41",
+    "id": "41",
     "displayName": "Cursus Integer Corporation",
     "orgURL": "cursusinteger",
     "city": "Rutland",
     "region": "Vermont"
   },
   {
-    "organizationID": "42",
+    "id": "42",
     "displayName": "Aliquet Nec Imperdiet Incorporated",
     "orgURL": "aliquetnec",
     "city": "Springdale",
     "region": "Arkansas"
   },
   {
-    "organizationID": "43",
+    "id": "43",
     "displayName": "Suspendisse Associates",
     "orgURL": "suspendisseassociates",
     "city": "Honolulu",
     "region": "HI"
   },
   {
-    "organizationID": "44",
+    "id": "44",
     "displayName": "Vestibulum Lorem Sit Inc.",
     "orgURL": "vestibulumlorem",
     "city": "New Orleans",
     "region": "LA"
   },
   {
-    "organizationID": "45",
+    "id": "45",
     "displayName": "Nullam Suscipit LLC",
     "orgURL": "nullamsuscipit",
     "city": "Provo",
     "region": "Utah"
   },
   {
-    "organizationID": "46",
+    "id": "46",
     "displayName": "Magna Praesent Associates",
     "orgURL": "magnapraesent",
     "city": "Flint",
     "region": "Michigan"
   },
   {
-    "organizationID": "47",
+    "id": "47",
     "displayName": "In LLP",
     "orgURL": "inllp",
     "city": "Newark",
     "region": "Delaware"
   },
   {
-    "organizationID": "48",
+    "id": "48",
     "displayName": "Sed Molestie PC",
     "orgURL": "sedmolestie",
     "city": "Kansas City",
     "region": "MO"
   },
   {
-    "organizationID": "49",
+    "id": "49",
     "displayName": "Eget Corporation",
     "orgURL": "egetcorporation",
     "city": "Cincinnati",
     "region": "OH"
   },
   {
-    "organizationID": "50",
+    "id": "50",
     "displayName": "Amet Consectetuer Adipiscing Industries",
     "orgURL": "ametconsectetuer",
     "city": "Madison",
     "region": "WI"
   },
   {
-    "organizationID": "51",
+    "id": "51",
     "displayName": "Et Company",
     "orgURL": "etcompany",
     "city": "Dover",
     "region": "DE"
   },
   {
-    "organizationID": "52",
+    "id": "52",
     "displayName": "Lorem Tristique Aliquet Associates",
     "orgURL": "loremtristique",
     "city": "Toledo",
     "region": "Ohio"
   },
   {
-    "organizationID": "53",
+    "id": "53",
     "displayName": "Duis Mi Enim Associates",
     "orgURL": "duismi",
     "city": "San Antonio",
     "region": "Texas"
   },
   {
-    "organizationID": "54",
+    "id": "54",
     "displayName": "Penatibus Et Magnis Industries",
     "orgURL": "penatibuset",
     "city": "Anchorage",
     "region": "AK"
   },
   {
-    "organizationID": "55",
+    "id": "55",
     "displayName": "Magna Foundation",
     "orgURL": "magnafoundation",
     "city": "Fayetteville",
     "region": "AK"
   },
   {
-    "organizationID": "56",
+    "id": "56",
     "displayName": "Nibh Aliquam Ornare Institute",
     "orgURL": "nibhaliquam",
     "city": "Iowa City",
     "region": "IA"
   },
   {
-    "organizationID": "57",
+    "id": "57",
     "displayName": "Odio Foundation",
     "orgURL": "odiofoundation",
     "city": "Waterbury",
     "region": "CT"
   },
   {
-    "organizationID": "58",
+    "id": "58",
     "displayName": "Consectetuer LLC",
     "orgURL": "consectetuerllc",
     "city": "Cheyenne",
     "region": "Wyoming"
   },
   {
-    "organizationID": "59",
+    "id": "59",
     "displayName": "Nibh Quisque Foundation",
     "orgURL": "nibhquisque",
     "city": "Montpelier",
     "region": "Vermont"
   },
   {
-    "organizationID": "60",
+    "id": "60",
     "displayName": "Eu Incorporated",
     "orgURL": "euincorporated",
     "city": "Anchorage",
     "region": "Alaska"
   },
   {
-    "organizationID": "61",
+    "id": "61",
     "displayName": "In Aliquet LLC",
     "orgURL": "inaliquet",
     "city": "Gresham",
     "region": "Oregon"
   },
   {
-    "organizationID": "62",
+    "id": "62",
     "displayName": "Vel PC",
     "orgURL": "velpc",
     "city": "Chicago",
     "region": "Illinois"
   },
   {
-    "organizationID": "63",
+    "id": "63",
     "displayName": "Gravida Nunc LLP",
     "orgURL": "gravidanunc",
     "city": "Lakewood",
     "region": "Colorado"
   },
   {
-    "organizationID": "64",
+    "id": "64",
     "displayName": "Donec PC",
     "orgURL": "donecpc",
     "city": "Portland",
     "region": "OR"
   },
   {
-    "organizationID": "65",
+    "id": "65",
     "displayName": "Convallis Limited",
     "orgURL": "convallislimited",
     "city": "Knoxville",
     "region": "TN"
   },
   {
-    "organizationID": "66",
+    "id": "66",
     "displayName": "Et Netus Corporation",
     "orgURL": "etnetus",
     "city": "Clarksville",
     "region": "TN"
   },
   {
-    "organizationID": "67",
+    "id": "67",
     "displayName": "Volutpat Corporation",
     "orgURL": "volutpatcorporation",
     "city": "Owensboro",
     "region": "KY"
   },
   {
-    "organizationID": "68",
+    "id": "68",
     "displayName": "Arcu Vestibulum Company",
     "orgURL": "arcuvestibulum",
     "city": "Idaho Falls",
-    "region": "organizationID"
+    "region": "id"
   },
   {
-    "organizationID": "69",
+    "id": "69",
     "displayName": "Non Consulting",
     "orgURL": "nonconsulting",
     "city": "Spokane",
     "region": "WA"
   },
   {
-    "organizationID": "70",
+    "id": "70",
     "displayName": "Purus Foundation",
     "orgURL": "purusfoundation",
     "city": "Sterling Heights",
     "region": "MI"
   },
   {
-    "organizationID": "71",
+    "id": "71",
     "displayName": "Donec Limited",
     "orgURL": "doneclimited",
     "city": "Flint",
     "region": "MI"
   },
   {
-    "organizationID": "72",
+    "id": "72",
     "displayName": "Risus Donec Egestas Company",
     "orgURL": "risusdonec",
     "city": "Columbus",
     "region": "Ohio"
   },
   {
-    "organizationID": "73",
+    "id": "73",
     "displayName": "Donec Industries",
     "orgURL": "donecindustries",
     "city": "Naperville",
     "region": "IL"
   },
   {
-    "organizationID": "74",
+    "id": "74",
     "displayName": "Quis Massa Mauris Consulting",
     "orgURL": "quismassa",
     "city": "Jackson",
     "region": "MS"
   },
   {
-    "organizationID": "75",
+    "id": "75",
     "displayName": "Eleifend Non Limited",
     "orgURL": "eleifendnon",
     "city": "Indianapolis",
     "region": "Indiana"
   },
   {
-    "organizationID": "76",
+    "id": "76",
     "displayName": "Pede Cras Vulputate Institute",
     "orgURL": "pedecrasv",
     "city": "Lafayette",
     "region": "LA"
   },
   {
-    "organizationID": "77",
+    "id": "77",
     "displayName": "Pellentesque Company",
     "orgURL": "pellentesquecompany",
     "city": "Lowell",
     "region": "MA"
   },
   {
-    "organizationID": "78",
+    "id": "78",
     "displayName": "Donec Elementum Lorem Ltd",
     "orgURL": "donecelementum",
     "city": "Springfield",
     "region": "Illinois"
   },
   {
-    "organizationID": "79",
+    "id": "79",
     "displayName": "Blandit Industries",
     "orgURL": "blanditindustries",
     "city": "Dallas",
     "region": "Texas"
   },
   {
-    "organizationID": "80",
+    "id": "80",
     "displayName": "Massa Non Ante Limited",
     "orgURL": "massanon",
     "city": "San Jose",
     "region": "California"
   },
   {
-    "organizationID": "81",
+    "id": "81",
     "displayName": "Vitae Purus Incorporated",
     "orgURL": "vitaepurus",
     "city": "Columbus",
     "region": "Ohio"
   },
   {
-    "organizationID": "82",
+    "id": "82",
     "displayName": "Aptent Taciti Institute",
     "orgURL": "aptenttaciti",
     "city": "Lafayette",
     "region": "Louisiana"
   },
   {
-    "organizationID": "83",
+    "id": "83",
     "displayName": "At Pede Associates",
     "orgURL": "atpede",
     "city": "Mobile",
     "region": "AL"
   },
   {
-    "organizationID": "84",
+    "id": "84",
     "displayName": "Vitae Posuere At Foundation",
     "orgURL": "vitaeposuere",
     "city": "Bellevue",
     "region": "Nebraska"
   },
   {
-    "organizationID": "85",
+    "id": "85",
     "displayName": "Bibendum Institute",
     "orgURL": "bibenduminstitute",
     "city": "Norman",
     "region": "Oklahoma"
   },
   {
-    "organizationID": "86",
+    "id": "86",
     "displayName": "Aliquam Rutrum Lorem LLP",
     "orgURL": "aliquamrutrum",
     "city": "Bellevue",
     "region": "Washington"
   },
   {
-    "organizationID": "87",
+    "id": "87",
     "displayName": "Et Ipsum Cursus PC",
     "orgURL": "etipsum",
     "city": "Stamford",
     "region": "CT"
   },
   {
-    "organizationID": "88",
+    "id": "88",
     "displayName": "Lobortis Class Associates",
     "orgURL": "lobortisclass",
     "city": "Juneau",
     "region": "AK"
   },
   {
-    "organizationID": "89",
+    "id": "89",
     "displayName": "Et Eros Corp.",
     "orgURL": "eteros",
     "city": "Anchorage",
     "region": "AK"
   },
   {
-    "organizationID": "90",
+    "id": "90",
     "displayName": "Erat Volutpat Industries",
     "orgURL": "eratvolutpat",
     "city": "Kansas City",
     "region": "Kansas"
   },
   {
-    "organizationID": "91",
+    "id": "91",
     "displayName": "Pretium Et Rutrum Corporation",
     "orgURL": "pretiumet",
     "city": "Gaithersburg",
     "region": "MD"
   },
   {
-    "organizationID": "92",
+    "id": "92",
     "displayName": "Dui Semper Inc.",
     "orgURL": "duisemper",
     "city": "Olympia",
     "region": "Washington"
   },
   {
-    "organizationID": "93",
+    "id": "93",
     "displayName": "Ornare PC",
     "orgURL": "ornarepc",
     "city": "Boston",
     "region": "MA"
   },
   {
-    "organizationID": "94",
+    "id": "94",
     "displayName": "Cras Limited",
     "orgURL": "craslimited",
     "city": "San Francisco",
     "region": "California"
   },
   {
-    "organizationID": "95",
+    "id": "95",
     "displayName": "Purus Mauris Company",
     "orgURL": "purusmauris",
     "city": "Bridgeport",
     "region": "Connecticut"
   },
   {
-    "organizationID": "96",
+    "id": "96",
     "displayName": "Ut Nisi PC",
     "orgURL": "utnisi",
     "city": "Fort Collins",
     "region": "CO"
   },
   {
-    "organizationID": "97",
+    "id": "97",
     "displayName": "Elit Associates",
     "orgURL": "elitassociates",
     "city": "Colchester",
     "region": "Vermont"
   },
   {
-    "organizationID": "98",
+    "id": "98",
     "displayName": "Ut Foundation",
     "orgURL": "utfoundation",
     "city": "Chattanooga",
     "region": "Tennessee"
   },
   {
-    "organizationID": "99",
+    "id": "99",
     "displayName": "Dolor Corporation",
     "orgURL": "dolorcorporation",
     "city": "Madison",
     "region": "Wisconsin"
   },
   {
-    "organizationID": "100",
+    "id": "100",
     "displayName": "Molestie Tellus Aenean LLC",
     "orgURL": "molestietellus",
     "city": "Sterling Heights",
